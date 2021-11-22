c684147551a67dd40fc69ab02f9de9a13f704ace
Romenick Tester <romenick.tester@gmail.com>
2021-11-22 22:15:06 +0000

    get current user profile route

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,27 @@
 import express from "express";
 
+import auth from "../middlewares/auth.js";
+import Profile from "../models/Profile.js";
+// import User from "../models/User.js";
+
 const router = express.Router();
 
 // @route   GET api/profile
-// @desc    test route
+// @desc    Get current user profile
 // @access  Public
-router.get("/", (req, res) => res.send("profile route"))
+router.get("/me", auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate("user", ["name", "avatar"]);
+
+        if (!profile) {
+            return res.status(400).json({ errors: [{ msg: "There no profile for this user." }] });
+        }
+
+        res.send("test profile route");
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error")
+    }
+})
 
 export default router;
