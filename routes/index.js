e17135fe825807192b7f68deb61e7fdfd3bcc9fe
Romenick Tester <romenick.tester@gmail.com>
2021-11-22 00:38:07 +0000

    create initial routes

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const auth = require("./auth");
+const posts = require("./posts");
+const profile = require("./profile");
+const users = require("./users");
+
+module.exports = {
+    auth,
+    posts,
+    profile,
+    users
+}
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const route = require("./routes");
 
 dotenv.config();
 
 const app = express();
 
 connectDB();
+
+app.use("/api/users", route.users);
+app.use("/api/posts", route.posts);
+app.use("/api/profile", route.profile);
+app.use("/api/auth", route.auth);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}...`));
