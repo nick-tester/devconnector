ee5f01ffada55f8cf54949ed6438aa9a740844b5
Romenick Tester <romenick.tester@gmail.com>
2022-01-10 10:01:36 +0000

    client: navbar refactor

--- client/src/layouts/Navbar.js
+++ client/src/layouts/Navbar.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { FaSignOutAlt, FaUser, FaSign, FaSignInAlt } from "react-icons/fa";
+import { FaSignOutAlt, FaUser, FaPen, FaSignInAlt } from "react-icons/fa";
 
 import { logout } from "../assets/reducers/auth_actions";
 
@@ -19,19 +19,22 @@ const Navbar = () => {
                 <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
             </h1>
             <ul>
-                <li><Link to="/profiles">Developers</Link></li>
+                <li>
+                    <Link to="/profiles">
+                        dev<span className="hide-sm">eloper</span>s
+                    </Link></li>
                 {isAuthenticated ? (
                     <>
                         <li>
                             <Link to="/auth/dashboard">
-                                <span className="hide-sm"></span>{user ? user.name : "User"}
-                                <FaUser />
+                                <span className="hide-lg" title="user"><FaUser /></span>
+                                <span className="hide-sm"> {user ? user.name : "user"}</span>
                             </Link>
                         </li>
                         <li>
                             <Link to="/auth/signin" onClick={logoutHandler}>
-                                <span className="hide-sm">Logout</span>
-                                <FaSignOutAlt />
+                                <span className="hide-lg" title="logout"><FaSignOutAlt /></span>
+                                <span className="hide-sm"> logout</span>
                             </Link>
                         </li>
                     </>
@@ -39,14 +42,14 @@ const Navbar = () => {
                     <>
                         <li>
                             <Link to="/auth/signup">
-                                <span className="hide-sm">Register</span>
-                                <FaSign />
+                                <span className="hide-lg" title="register"><FaPen /></span>
+                                <span className="hide-sm"> signup</span>
                             </Link>
                         </li>
                         <li>
                             <Link to="/auth/signin">
-                                <span className="hide-sm">Login</span>
-                                <FaSignInAlt />
+                                <span className="hide-lg" title="login"><FaSignInAlt /></span>
+                                <span className="hide-sm"> signin</span>
                             </Link>
                         </li>
                     </>
